98aa982890fc5bb4072627f3f6d3335c0394dab8
Renee Do <reneedo1000@gmail.com>
2026-03-12 15:52:28 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,11 +3,11 @@
 # Fix them and submit a pull request!
 
 def add(a, b):
-    return a - b  
+    return a + b  
 
 
 def subtract(a, b):
-    return a + b   
+    return a - b   
 
 
 def multiply(a, b):
@@ -15,7 +15,7 @@ def multiply(a, b):
 
 
 def divide(a, b):
-    return a * b  
+    return a / b  
 
 
 def main():
